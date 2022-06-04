0dbf70911b183898b1d61beda0fa2f041eee7b52
RudyMathis <103608310+RudyMathis@users.noreply.github.com>
2022-06-04 17:24:01 -0400

    Did:
    Added functionality to menu with JS
    Restructured HTML
    Cleaned up CSS
    
    Need to do:
    
    Add animation to menu
    Make responsive
    Fix minor CSS inconsistancies

--- navigation.js
+++ navigation.js
@@ -1,27 +1,28 @@
 const menu = document.querySelector(".menu");
 const btn = document.querySelector(".btn");
-const body = document.querySelector("body");
-const main = document.querySelector("main");
 const menuClose = document.querySelector(".menu-close");
-const btnParent = document.querySelectorAll(".btn-parent");
 const listParent = document.querySelectorAll(".list-parent");
+const modal = document.querySelector(".modal");
 
 
 btn.addEventListener("click", ()=>{
-    menu.classList.toggle("hide");
-    menuClose.classList.toggle("hide");
-    // main.style.filter = "brightness(.1)"
+    toggleFunction(menu, menuClose, modal);
 });
 
 
 listParent.forEach((btn) => {
     btn.addEventListener('click', (e) => {
         answer = btn.lastElementChild;
-        console.log(answer)
-        answer.classList.toggle('hide');
-        // arrow = btn.lastElementChild;
-        // arrow.classList.toggle("flip");
-        // bold = btn.firstElementChild;
-        // bold.classList.toggle("bold");
+        flipUP = btn.firstElementChild.firstElementChild;
+        flipDown = btn.firstElementChild.firstElementChild.nextElementSibling;
+
+        toggleFunction(answer, flipUP, flipDown);
+
     });
-    });
+});
+
+function toggleFunction(a, b, c){
+    a.classList.toggle("hide");
+    b.classList.toggle("hide");
+    c.classList.toggle("hide");
+}
